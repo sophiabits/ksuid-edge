e6b3c77cec42a47d8b1f97d332f27626714c92c6
Sophia Willows <20146550+sophiabits@users.noreply.github.com>
2023-07-27 22:53:39 +1200

    `fixedLength` params are optional

--- src/base62.ts
+++ src/base62.ts
@@ -2,7 +2,7 @@ import baseConvertIntArray from 'base-convert-int-array';
 
 const BASE62 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-export function base62(view: DataView, fixedLength: number): string {
+export function base62(view: DataView, fixedLength?: number): string {
   const numbs = new Array(view.byteLength);
   for (let offset = 0; offset < view.byteLength; offset++) {
     numbs[offset] = view.getUint8(offset);
@@ -14,7 +14,7 @@ export function base62(view: DataView, fixedLength: number): string {
 }
 
 // https://github.com/novemberborn/ksuid/blob/90ca4c1508f216e03923de610291786a0d6a868c/base62.js#L13C40-L21C85
-export function debase62(data: string, fixedLength: number) {
+export function debase62(data: string, fixedLength?: number) {
   const input = Array.from(data, (char) => {
     const charCode = char.charCodeAt(0);
     if (charCode < 58) return charCode - 48;
