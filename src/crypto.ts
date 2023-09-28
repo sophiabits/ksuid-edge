f982c412f0938577d6744f152d1649aae6dece3b
bae <73325521+pcbzzz@users.noreply.github.com>
2023-09-28 19:58:39 +0000

    Use `node:crypto` on non-node runtimes

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,6 +1,6 @@
 function getCrypto() {
   if (typeof crypto === 'undefined') {
-    return require('crypto').webcrypto as Crypto;
+    return require('node:crypto').webcrypto as Crypto;
   }
 
   return crypto;
